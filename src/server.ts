5a2a8267beed51610f2c6806209e5885bc67bd03
developerdesigner18 <developer.designer18@gmail.com>
2025-06-30 16:25:34 +0530

    Git-staged

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ function helloword(name: string) {
     console.log(`Hello ${name}`);
 }
 function helloWorld(name: string) {
-    // console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
 }
 
 helloword('Neel Patel');
