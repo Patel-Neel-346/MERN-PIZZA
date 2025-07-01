0600bf2b252b152d7923940e150bd5def776f78e
developerdesigner18 <developer.designer18@gmail.com>
2025-07-01 10:25:18 +0530

    fix: enhance error handling and improve express app structure

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import logger from './config/logger';
 
 const app = express();
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (_req: express.Request, res: express.Response) => {
     res.send('Hello World Neel Patel! HI');
+});
+
+//global error handler and alwayes last in all routes
+//added ERROR Handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, _req: Request, res: Response, _next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
