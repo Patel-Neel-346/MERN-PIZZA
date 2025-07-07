f53cff55d01e2ca5b7c6b0f66604851a383d248f
developerdesigner18 <developer.designer18@gmail.com>
2025-07-07 13:01:33 +0530

    feat: add global error handler middleware and integrate UUID for error logging

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { HttpError } from 'http-errors';
 import logger from './config/logger';
 import UserRouter from './routes/UserRoutes';
 import cookieParser from 'cookie-parser';
+import { GlobalErrorHandler } from './middleware/GlobalErrorHandler';
 const app = express();
 
 app.use(express.json());
@@ -16,20 +17,22 @@ app.use('/auth', UserRouter);
 //global error handler and alwayes last in all routes
 //added ERROR Handler
 
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message);
-    const statusCode = err.statusCode || 500;
+// app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+//     logger.error(err.message);
+//     const statusCode = err.statusCode || 500;
 
-    res.status(statusCode).json({
-        errors: [
-            {
-                type: err.name,
-                msg: err.message,
-                path: '',
-                location: '',
-            },
-        ],
-    });
-});
+//     res.status(statusCode).json({
+//         errors: [
+//             {
+//                 type: err.name,
+//                 msg: err.message,
+//                 path: '',
+//                 location: '',
+//             },
+//         ],
+//     });
+// });
 
+//global error handler
+app.use(GlobalErrorHandler);
 export default app;
---
+++ src/middleware/GlobalErrorHandler.ts
@@ -0,0 +1,35 @@
+import { HttpError } from 'http-errors';
+import { Request, Response, NextFunction } from 'express';
+import logger from '../config/logger';
+import { v4 as uuidv4 } from 'uuid';
+export const GlobalErrorHandler = (
+    err: HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const errorId = uuidv4();
+    const statusCode = err.statusCode || err.status || 500;
+    const isProduction = process.env.NODEENV === 'prod';
+    const message = isProduction ? 'Internel server Error' : err.message;
+    logger.error(err.message, {
+        id: errorId,
+        statusCode,
+        error: err.stack,
+        path: req.path,
+        method: req.method,
+    });
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: message,
+                path: req.path,
+                location: 'server',
+                method: req.method,
+                stack: isProduction ? null : err.stack,
+            },
+        ],
+    });
+};
