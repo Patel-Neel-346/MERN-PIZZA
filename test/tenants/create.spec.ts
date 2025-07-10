38d3cb73c526a6331b46bb7fc07879bbcc90ddf1
developerdesigner18 <developer.designer18@gmail.com>
2025-07-10 15:28:27 +0530

    feat:added Tenants Testing code in jest

---
+++ test/tenants/create.spec.ts
@@ -0,0 +1,79 @@
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../src/config/data-source';
+import request from 'supertest';
+import app from '../../src/app';
+import { response } from 'express';
+import { Tenant } from '../../src/entity/Tenants';
+
+describe('POST /tenants', () => {
+    let connection: DataSource;
+
+    //connection to DB
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    //before each api call  clear Prev Database Data from DB
+    beforeEach(async () => {
+        await connection?.dropDatabase();
+        await connection?.synchronize();
+    });
+
+    //distory connection after exits Last option
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    //happy path
+    describe('Given All Fields', () => {
+        it('should return 201 status code', async () => {
+            //arrange
+
+            const tenantData = {
+                name: 'Tenant Name',
+                address: 'Tenant Address',
+            };
+
+            //act
+
+            const Response = await request(app)
+                .post('/tenants')
+                .send(tenantData);
+
+            //assert
+
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('should create tenant in database', async () => {
+            const tenantData = {
+                name: 'Tenant Name',
+                address: 'Tenant Address',
+            };
+
+            //act
+
+            const Response = await request(app)
+                .post('/tenants')
+                .send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(1);
+            expect((Response.body as Record<string, string>).name).toBe(
+                tenantData.name,
+            );
+        });
+
+        it('should return 401 if user is not authenticated!', async () => {});
+
+        it('should return 403 if user is not admin', async () => {});
+    });
+
+    //sad path
+
+    describe('Fields Are missing !', () => {
+        it('should return 400 if fields are missing', async () => {});
+    });
+});
